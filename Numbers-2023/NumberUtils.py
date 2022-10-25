7a4c94fb18fbd59ea88fe4fa0737205653ed531e
Heidi Schellman <heidi.schellman@oregonstate.edu>
2022-10-25 10:52:39 -0700

    full chain

---
+++ Numbers-2023/NumberUtils.py
@@ -0,0 +1,204 @@
+#NumberUtils.py
+
+# Utility function: object = cummulate(object,lifetime)
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import math
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
+                               AutoMinorLocator)
+import numpy as np
+
+def makeArray(map):
+   
+    years = list(map.keys())
+    array = np.zeros(len(years))
+    i = 0
+    for year in years:
+        array[i] = map[year]
+        i +=1
+   
+    return array
+
+
+
+# Utility function: string = dump(datatype,det,object)
+
+# make a dump of a record
+
+def dump(det,datatype,a,Units):
+  s = ""
+  s  += "%5s, %10s (%s)"%(det,datatype,Units[datatype])
+  for j in range(0,len(a)):
+    s += ", "
+    s += "%8.1f"%a[j]
+  s += "\n"
+  return s
+
+def ToCSV1(name,first,second,values,Units): # loop over first index to make csv n
+    f = open(name+".csv",'w')
+    # needs first as a hint to get the right secondary index set
+    print (first, values[first].keys())
+    years = values[first][second].keys()
+
+    comma = "\t,"
+    s = second + comma
+    for year in years:
+        s += "%d \t,"%year
+    s += "\n"
+    f.write(s)
+    print ("CSV1", list(values.keys()))
+    for l in list(values.keys()):
+        if not second in values[l].keys(): # sorry, this type doesn't have this key
+            print ("no such key", first, second)
+            continue
+        s = l + "(%s)"%Units[second] + comma
+        v = values[l][second]
+        for y in years:
+            s += "%d \t,"%values[l][second][y]
+        s +="\n"
+        f.write(s)
+    f.close
+        
+ 
+def ToCSV2(name,first,values,Units): # loop over second index to make csv
+    f = open(name+".csv",'w')
+    second = list(values[first].keys())[0]
+    years = values[first][second].keys()
+    loop = list(values[first].keys())
+    comma = "\t,"
+    s = first + comma
+    for year in years:
+        s += "%d \t,"%year
+    s += "\n"
+    f.write(s)
+    for l in loop:
+        s = l + " (%s)"%Units[first] + comma
+        v = values[first][l]
+        for y in years:
+            s += "%d \t,"%values[first][l][y]
+        s +="\n"
+        f.write(s)
+    f.close
+   
+
+# sum backwards over lifetime of record
+def cumulateMap(years,a,lifetime=100):
+  lifetimebase = math.floor(lifetime)
+  lifetimeextra = lifetime - lifetimebase
+  #print(lifetimeextra)
+  b = {}
+  for i in years:
+      b[i] = 0.0
+
+  if lifetimeextra == 0:
+      for i in years:
+        begin = max(years[0],i-lifetimebase+1)
+        for j in range(begin,i+1):
+          b[i] += a[j]
+ # here for partial years.
+  else:
+      for i in years:
+        begin = max(years[0],i-lifetimebase+1)
+        for j in range(begin,i+1):
+          b[i] += a[j]
+        if begin > years[0]:
+            #print ("fix for partial",i,a[i],b[i],a[begin-1],b[i]+a[begin-1]*lifetimeextra)
+            b[i]+= a[begin-1]*lifetimeextra
+  #print ("cumulateMap",a,b)
+  #print (b)
+  return b
+
+# Utility function: DrawDet(Value,Years,Data,Types,Units,detcolors,detlines)
+
+# for detector values
+def DrawDet(Name,Value,InYears,Data,Types,Units,detcolors,detlines,points=None):
+    #print (InYears)
+    Years = np.array(InYears)
+    #print (Years)
+    maxyears = Years[len(Years)-1]
+
+    fig=plt.figure()
+    ax = fig.add_axes([0.2,0.2,0.7,0.7])
+    ax.set_xlim(Years[0],maxyears)
+    if len(Years)<10:
+      ax.xaxis.set_major_locator(MultipleLocator(1))
+    else:
+      ax.xaxis.set_major_locator(MultipleLocator(5))
+    ax.spines['bottom'].set_position('zero')
+    #print (Data)
+    toplot = Data[Value]
+    for type in Types:
+      if type not in detcolors:
+        print (type, "not in ",detcolors)
+      else:
+        ypoints = makeArray(toplot[type])
+        print ("y points",Value,type,ypoints)
+        ax.plot(Years,ypoints,color=detcolors[type],linestyle=detlines[type],label="model "+type)
+    if points != None:
+        for y in points:
+            for t in points[y]:
+                print ("t is ",t)
+                if t in detcolors:
+                  ax.plot(y,points[y][t],color=detcolors[t],marker="s",label="actual "+ t,markerfacecolor='none')
+                else:
+                  print (t ,"not in ", detcolors)
+    ax.legend(frameon=False)
+    ax.set_title(Value)
+    ax.set_xlabel("Year")
+    ax.set_ylabel(Value + ", " + Units[Value])
+    plt.grid()
+    plt.savefig(Name+"-"+Value.replace(" ","-")+".png",transparent=True)
+    #plt.savefig(Value+"_w.jpg",transparent=False)
+
+    plt.show()
+
+
+# Utility function: DrawType(Value,Years,Data,Types,Units,typecolors,typelines)
+
+# draw by data type (transpose of the detectors)
+
+def DrawType(Name,Value,Years,Data,Types,Units,typecolors,typelines,points=None,contributions=None):
+    fig=plt.figure()
+    ax = fig.add_axes([0.2,0.2,0.7,0.7])
+    maxyears = Years[-1]
+    ax.set_xlim(Years[0],maxyears)
+    if len(Years)<10:
+      ax.xaxis.set_major_locator(MultipleLocator(1))
+    else:
+      ax.xaxis.set_major_locator(MultipleLocator(5))
+    ax.spines['bottom'].set_position('zero')
+    
+    
+    for type in Types:
+      ypoints = makeArray(Data[type][Value])
+      ax.plot(Years,ypoints,color=typecolors[type],linestyle=typelines[type],label="model "+type)
+    if points != None:
+        for y in points:
+            for t in points[y]:
+                ypoints = makeArray(Data[t][Value])
+                ax.plot(y,ypoints,color=typecolors[t],marker="o",label="actual "+t,markerfacecolor='none')
+
+    if contributions != None:
+        for y in contributions:
+            for t in contributions[y]:
+                if t in typecolors:
+                  ax.plot(y,contributions[y][t],color=typecolors[t],marker="s",label="actual  "+t)
+                else:
+                  print (" no such color",t, typecolors)
+    ax.legend(frameon=False)
+    ax.set_xlabel("Year")
+    ax.set_ylabel(Value + ", " + Units[Value])
+    ax.set_title(Value)
+    plt.grid()
+    plt.savefig(Name + "-"+Value.replace(" ","-")+".png",transparent=True)
+    #plt.savefig(Value+"_w.jpg",transparent=False)
+
+    plt.show()
+
+def DrawTex(shortname,figure,caption,label):
+    s = "\\begin{figure}[h]\n\\centering"
+    s += "\\includegraphics[height=0.4\\textwidth]{%s-%s}"%(shortname,figure)
+    s += "\\label{%s}\n"%label
+    s += "\\caption{%s}\n"%caption
+    s += "\\end{figure}\n"
+    return s
--- Numbers-2023/Numbers-2023-10-23.py
+++ Numbers-2023/Numbers-2023-10-23.py
@@ -3,6 +3,7 @@ from csv import reader
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
+DEBUG = True
 
 import numpy as np
 
@@ -26,7 +27,7 @@ else:
   print ("no config file",configfile)
   sys.exit(0)
 
-if not "Version" in config or config["Version"] < 2:
+if not "Version" in config or config["Version"] < 5:
   print (" this code expects Version >= 2")
   sys.exit(1)
 
@@ -36,7 +37,11 @@ MaxYear = config["MaxYear"]
 
 MinYear = config["MinYear"]
 Detectors = config["Detectors"]
+if DEBUG:
+  Detectors = ["SP","SP2","DP"]
 Years = np.array(config["Years"])
+if DEBUG:
+  Years = Years[0:7]
 shortname = shortname.replace("2040","%d"%MaxYear)
 print (Years, len(Years))
 size = len(Years)
@@ -90,6 +95,7 @@ for det in Detectors:
 
 # fill in other useful arrays
 for det in Detectors:
+  if DEBUG: print ("Events",det,Inputs[det]["Events"])
   for key in DetectorParameters:
     # skip the ones already done
     if key in dofirst:
@@ -102,16 +108,18 @@ for det in Detectors:
             for year in Years:
                 Inputs[det][key][year] *= config[det][key]
       else:
+        if key == "Raw":
             Inputs[det][key] ={}
             for year in Years:
                 Inputs[det][key][year] = Inputs[det]["Events"][year]*config[det][key]
+        else:
+            continue
     else:
         Inputs[det][key] ={}
         for year in Years:
             Inputs[det][key][year]=Inputs[det]["Sim Events"][year]*config[det][key]
 
-    print ("Inputs",det,key,Inputs[det][key][2022])
-
+    
 if PerYear["Reco"]!=PerYear["CPU"]:
     print ("Data growth has to match reprocessing cycles/year")
     PerYear["Reco"] = PerYear["CPU"]
@@ -120,6 +128,7 @@ if PerYear["Sim"]!=PerYear["Sim-CPU"]:
     PerYear["Sim"] = PerYear["Sim-CPU"]
 
 Data = {}
+dump = open("dump.txt",'w')
 #print (Inputs.keys())
 fields = list(Inputs["ND"].keys())
 for dtype in fields:
@@ -133,11 +142,19 @@ for dtype in fields:
     if Units[dtype] == "PB":
         for year in Years:
             Data[dtype][det][year] *= 0.001
-    print ("data",dtype,det,Data[dtype][det][2022])
-
+    ds = "data %s %s %f\n"%(dtype,det,Data[dtype][det][2022])
+    dump.write(ds)
 PlotYears = []
 for i in range(MinYear,MaxYear-1):
     PlotYears.append(i)
 PlotYears = Years
 
 DrawDet(shortname,"Events",PlotYears,Data,Inputs.keys(),Units,DetColors,DetLines)
+DrawDet(shortname,"CPU",Years,Data,Inputs.keys(),Units,DetColors,DetLines)
+DrawDet(shortname,"Raw",Years,Data,Inputs.keys(),Units,DetColors,DetLines)
+DrawDet(shortname,"Test",Years,Data,Inputs.keys(),Units,DetColors,DetLines)
+DrawDet(shortname,"Sim Events",Years,Data,Inputs.keys(),Units,DetColors,DetLines)
+DrawDet(shortname,"Sim",Years,Data,Inputs.keys(),Units,DetColors,DetLines)
+DrawDet(shortname,"Reco",Years,Data,Inputs.keys(),Units,DetColors,DetLines)
+
+
