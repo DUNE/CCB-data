cf4ad4b0f7c22a6ff6e2e1cff812c116aea15500
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2024-08-14 11:11:58 -0700

    fix gitadd

--- model-2024/gitadd.sh
+++ model-2024/gitadd.sh
@@ -3,6 +3,7 @@ git add 2024-08-09-test.ipynb
 ../convert.sh 2024-08-09-test.ipynb
 git add 2024-08-09-test.py
 git add DataHolder.py
+git add NumberUtils.py
 git add *08-09*json
 git add *08-09*timeline.csv
 git add *08-14*json
